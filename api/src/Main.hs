fa0dfe3c012412ec7ec0a1dab17386e2acba7904
44castle <castlekerr@hotmail.com>
2018-03-01 17:41:51 -0600

    added JSON response and MySQL connection

--- api/src/Main.hs
+++ api/src/Main.hs
@@ -1,24 +1,47 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 module Main where
 
-import           Control.Applicative
-import           Snap.Core
-import           Snap.Util.FileServe
-import           Snap.Http.Server
+import Data.Maybe
+import GHC.Generics
+import Control.Monad.IO.Class
+import Snap.Core hiding (pass)
+import Snap.Http.Server
+import Data.Text (Text)
+import Data.Aeson
+--import Data.Configurator
+import Database.MySQL.Base
+import System.IO.Streams (toList)
+
+data User = User
+    { userName :: Text
+    , firstName :: Text
+    , lastName :: Text
+    } deriving (Generic, Show)
+
+instance ToJSON User where
 
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
-site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
+site = do
+    modifyResponse (setContentType "application/json")
+    users <- liftIO getUsers
+    mapM_ (writeLBS . encode) users
 
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+getUsers :: IO [User]
+getUsers = do
+    --config <- load [Required "database.cfg"]
+    --dbUser <- require config "user"
+    --dbPass <- require config "password"
+    let dbUser = "root"
+    let dbPass = "test"
+    conn <- connect defaultConnectInfo { ciUser = dbUser, ciPassword = dbPass, ciDatabase = "territories" }
+    (_, resultStream) <- query_ conn "SELECT username, firstName, lastName FROM user"
+    users <- catMaybes . map toUser <$> toList resultStream
+    close conn
+    return users
+
+toUser :: [MySQLValue] -> Maybe User
+toUser [MySQLText u, MySQLText f, MySQLText l] = Just (User u f l)
+toUser _                                       = Nothing
